2b9b784a04494999e9b7cdc31de83c6518c06a88
omelendrez <omar.melendrez@gmail.com>
2021-03-27 17:26:07 -0300

    Created one component for the new app using antd as ui library

---
+++ src/components/transportes/Transportes.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Layout, Table } from 'antd';
+import Header from '../Header';
+import { getRecords } from '../../services';
+const { columns } = require(`./columns`);
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+};
+
+const Transportes = () => {
+
+  const [records, setRecords] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getRecords('transportes')
+      .then(data => {
+        setRecords(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <Layout>
+      <Header title="Transportes" />
+      <Table
+        loading={loading}
+        columns={columns}
+        dataSource={records}
+        rowSelection={{ ...rowSelection }}
+        rowKey="ID"
+        pagination={{ position: ['bottomCenter'] }}
+        size="small"
+        sticky={true}
+        tableLayout="fixed"
+      />
+    </Layout>
+  );
+};
+
+export default Transportes;
---
+++ src/services/index.js
@@ -0,0 +1,47 @@
+import api from "./api";
+import { handleError } from "../utils/helpers";
+
+export const getRecords = endpoint => {
+  return new Promise((resolve, reject) => {
+    api
+      .get(endpoint)
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(handleError(error)));
+  });
+};
+
+export const getRecordById = (endpoint, id) => {
+  return new Promise((resolve, reject) => {
+    api
+      .get(`${endpoint}/${id}`)
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(handleError(error)));
+  });
+};
+
+export const addRecord = (endpoint, record) => {
+  return new Promise((resolve, reject) => {
+    api
+      .post(endpoint, record)
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(handleError(error)));
+  });
+};
+
+export const updateRecord = (endpoint, record) => {
+  return new Promise((resolve, reject) => {
+    api
+      .put(`${endpoint}/${record.ID}`, record)
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(handleError(error)));
+  });
+};
+
+export const deleteRecord = (endpoint, id) => {
+  return new Promise((resolve, reject) => {
+    api
+      .delete(`${endpoint}/${id}`)
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(handleError(error)));
+  });
+};
