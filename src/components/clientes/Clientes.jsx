6e94917c4a820c177990d0b696f3143e67179aa9
omelendrez <omar.melendrez@gmail.com>
2021-04-07 21:13:31 -0300

    Add Nuevo cliente in clientes table

--- src/components/clientes/Clientes.jsx
+++ src/components/clientes/Clientes.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "antd";
 import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 import Header from "../common/Header";
 import Table from "../common/Table";
 import Alert from "../common/Alert";
@@ -10,18 +11,28 @@ const { columns } = require(`./columns`);
 const Clientes = () => {
   const dispatch = useDispatch();
   const clientes = useSelector((state) => state.clientes);
+  const [url, setUrl] = useState("");
   const { loading, records, error } = clientes;
 
   useEffect(() => {
     dispatch(getClientes());
   }, [dispatch]);
 
+  const onAdd = () => {
+    setUrl(`/clientes/add/cliente`);
+  };
+
   const tableProps = {
     loading,
     columns,
     dataSource: records,
     rowKey: "ID",
+    onAdd,
   };
+
+  if (!!url) {
+    return <Redirect push to={{ pathname: url, state: { record: {} } }} />;
+  }
 
   return (
     <Layout>
