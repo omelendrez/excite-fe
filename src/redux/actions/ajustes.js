5f3979803ad33c57ea3a3e8b5ef51681f5c908c1
matimancinelli <matiasmancinelli1998@hotmail.com>
2021-04-13 23:20:04 -0300

    resolves #38

--- src/redux/actions/ajustes.js
+++ src/redux/actions/ajustes.js
@@ -5,3 +5,32 @@ export const getAjustes = () => {
     type: types.GET_AJUSTES_REQUEST,
   };
 };
+
+export const getAjuste = (id) => {
+  return {
+    type: types.GET_AJUSTE_REQUEST,
+    id,
+  };
+};
+
+export const addAjuste = (newData) => {
+  return {
+    type: types.ADD_AJUSTE_REQUEST,
+    newData,
+  };
+};
+
+export const updateAjuste = (id, newData) => {
+  return {
+    type: types.UPDATE_AJUSTE_REQUEST,
+    id,
+    newData,
+  };
+};
+
+export const deleteAjuste = (id) => {
+  return {
+    type: types.DELETE_AJUSTE_REQUEST,
+    id,
+  };
+};
