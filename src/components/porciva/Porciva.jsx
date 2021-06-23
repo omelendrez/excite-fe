636087913e665c06640624b04e784dfcd1673509
omelendrez <omar.melendrez@gmail.com>
2021-06-23 09:07:48 -0300

    Optimize api calls for porciva endpoint!

--- src/components/porciva/Porciva.jsx
+++ src/components/porciva/Porciva.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Layout } from "antd";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Header from "components/common/Header";
 import Table from "components/common/Table";
 import Alert from "components/common/Alert";
-import { getPorciva } from "redux/actions";
 const { columns } = require(`./columns`);
 
 const Porciva = () => {
-  const dispatch = useDispatch();
   const porciva = useSelector((state) => state.porciva);
   const { loading, records, error } = porciva;
-
-  useEffect(() => {
-    dispatch(getPorciva());
-  }, [dispatch]);
 
   const tableProps = {
     loading,
