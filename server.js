ffad4af8bda506ded739d0006645f595875b6b08
omelendrez <omar.melendrez@gmail.com>
2021-07-19 22:35:39 -0300

    Rename endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use((req, res, next) => {
     if (req.header("x-forwarded-proto") !== "https") {
-      res.redirect(`https://${req.header("host")}${req.baseUrl}`);
+      res.redirect(`https://${req.header("host")}${req.url}`);
     } else {
       next();
     }
