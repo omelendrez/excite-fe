2d3ef79c4631cd62c7670075155bafb38341d868
omelendrez <omar.melendrez@gmail.com>
2021-08-15 09:40:35 -0300

    Remove innecessary sorting import

--- src/components/conceptos/columns.js
+++ src/components/conceptos/columns.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { formatAmount, formatDate, sortColumn } from "utils/helpers";
+import { formatAmount, formatDate } from "utils/helpers";
 
 const columns = () => [
   {
