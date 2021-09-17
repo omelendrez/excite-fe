5552c2990d18a2e145e61d2e0d6bb0f4e2533842
omelendrez <omar.melendrez@gmail.com>
2021-09-16 21:01:49 -0300

    Add env var

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     Accept: "application/json",
     "Content-Type": "application/json",
   },
-  baseURL: process.env.REACT_APP_BACKEND_URL || "http://excite-pc:3000",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000",
 });
 
 export default api;
