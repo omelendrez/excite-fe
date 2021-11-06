cad0023d5aedf22d06caaf18209ef2173f482ed6
omelendrez <omar.melendrez@gmail.com>
2021-11-06 15:40:05 -0300

    Change save button and inherit text color in svg icon

--- src/components/common/SaveButton.jsx
+++ src/components/common/SaveButton.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button } from 'antd'
-import { SaveTwoTone } from '@ant-design/icons'
+import { CheckCircleTwoTone } from '@ant-design/icons'
 
 const SaveButton = (props) => {
   const saveButtonText = props.saveButtonText || 'Guardar'
   return (
     <Button
       type="primary"
-      icon={<SaveTwoTone />}
+      icon={<CheckCircleTwoTone />}
       shape="round"
       htmlType="submit"
       disabled={props.disabled}
