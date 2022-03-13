30c363cf07b971bd56c7dbca42cd15815bf97ec8
omelendrez <omar.melendrez@gmail.com>
2022-03-13 11:26:18 -0300

    Testing deployment

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     Accept: "application/json",
     "Content-Type": "application/json",
   },
-  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3001",
 });
 
 export default api;
